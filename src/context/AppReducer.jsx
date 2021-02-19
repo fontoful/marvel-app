6fea6fd2d444b7cb72e0217819c6180b7e7e9546
Hector Serrano <fonto.serrano@gmail.com>
2021-02-18 20:37:41 -0600

    remove comments

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -10,25 +10,3 @@ export const AppReducer = (state, action) => {
       return state
   }
 }
-
-// import { initialStateProps } from './GlobalContext'
-
-// type Action =
-//   | {
-//       type: 'ADD_CHAR_BOOKMARK'
-//     }
-//   | { type: 'REMOVE_CHAR_BOOKMARK' }
-
-// export const AppReducer = (state: initialStateProps, action: Action): any => {
-//   switch (action.type) {
-//     case 'ADD_CHAR_BOOKMARK': {
-//       return 'add'
-//     }
-//     case 'REMOVE_CHAR_BOOKMARK': {
-//       return 'remove'
-//     }
-//     default: {
-//       console.log('unhandled type')
-//     }
-//   }
-// }
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -45,36 +45,3 @@ export const GlobalProvider = ({ children }) => {
     </GlobalContext.Provider>
   )
 }
-
-// import { createContext, useReducer, useEffect } from 'react'
-
-// import { AppReducer } from './AppReducer'
-
-// //interface
-// import { IResultProps } from '../types/APItypes'
-
-// export const GlobalContext = createContext<initialStateProps | null>(null)
-
-// type GlobalProviderProps = {
-//   children: React.ReactNode
-// }
-
-// export type initialStateProps = {
-//   characterBookmarks: Partial<IResultProps>[]
-//   comicsBookmarks: Partial<IResultProps>[]
-// }
-
-// const initialState: initialStateProps = {
-//   characterBookmarks: localStorage.getItem('characterBookmarks')
-//     ? JSON.parse(localStorage.getItem('characterBookmarks'))
-//     : [],
-//   comicsBookmarks: localStorage.getItem('comicsBookmarks')
-//     ? JSON.parse(localStorage.getItem('comicsBookmarks'))
-//     : [],
-// }
-
-// export const GlobalProvider = ({ children }: GlobalProviderProps) => {
-//   const [state, dispatch] = useReducer(AppReducer, initialState)
-
-//   return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>
-// }
