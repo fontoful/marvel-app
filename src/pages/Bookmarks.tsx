10ad8f6e709fc426af8ae8bc97c11057be6c593b
Hector Serrano <fonto.serrano@gmail.com>
2021-02-19 20:47:27 -0600

    hot fixes

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from 'react'
+import { useContext } from 'react'
 
 // Components
 import Navigation from '../components/Navigation'
@@ -17,7 +17,60 @@ import { unifyString } from '../utils/helper'
 import { Link } from 'react-router-dom'
 
 const Bookmarks: React.FC = () => {
-  const { characterBookmarks, comicsBookmarks } = useContext(GlobalContext)
+  const {
+    characterBookmarks,
+    comicsBookmarks,
+    removeCharFromBookmarks,
+    addCharToBookmarks,
+    addComicToBookmarks,
+    removeComicFromBookmarks,
+  } = useContext(GlobalContext)
+
+  const checkBookmarkValidity = (id: number) => {
+    const found = characterBookmarks.find(
+      (character: IResultProps) => character.id === id,
+    )
+
+    if (found) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const checkComicValidity = (id: number) => {
+    const found = comicsBookmarks.find((comic: IResultProps) => comic.id === id)
+
+    if (found) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const bookMarkHandler = (char: IResultProps) => {
+    const found = characterBookmarks.find(
+      (character: IResultProps) => character.id === char.id,
+    )
+
+    if (found) {
+      return removeCharFromBookmarks(char.id)
+    } else {
+      return addCharToBookmarks(char)
+    }
+  }
+
+  const comicBookmarkHandler = (comic: IResultProps) => {
+    const found = comicsBookmarks.find(
+      (character: IResultProps) => character.id === comic.id,
+    )
+
+    if (found) {
+      return removeComicFromBookmarks(comic.id)
+    } else {
+      return addComicToBookmarks(comic)
+    }
+  }
 
   // Merge the two arrays
   // const mergedArray: IResultProps[] = [
@@ -35,7 +88,7 @@ const Bookmarks: React.FC = () => {
           </h1>
         ) : (
           <Col>
-            <h1>
+            <h1 className='bookmarks-header'>
               Find your Bookmarks here{' '}
               <i className='fas fa-arrow-circle-down'></i>
             </h1>
@@ -44,7 +97,17 @@ const Bookmarks: React.FC = () => {
                 <Card>
                   <Card.Img src={unifyString(char.thumbnail)} />
                   <Card.Body>
-                    <Card.Title>{char.name}</Card.Title>
+                    <Card.Title>
+                      {char.name}
+                      <i
+                        onClick={() => bookMarkHandler(char)}
+                        className={
+                          checkBookmarkValidity(char.id)
+                            ? 'fas fa-bookmark'
+                            : 'far fa-bookmark'
+                        }
+                      />
+                    </Card.Title>
                     <Link to={`/characters/${char.id}`}>
                       <Button variant='outline-info'>View it</Button>
                     </Link>
@@ -55,7 +118,17 @@ const Bookmarks: React.FC = () => {
                 <Card>
                   <Card.Img src={unifyString(comic.thumbnail)} />
                   <Card.Body>
-                    <Card.Title>{comic.title}</Card.Title>
+                    <Card.Title>
+                      {comic.title}
+                      <i
+                        onClick={() => comicBookmarkHandler(comic)}
+                        className={
+                          checkComicValidity(comic.id)
+                            ? 'fas fa-bookmark'
+                            : 'far fa-bookmark'
+                        }
+                      />
+                    </Card.Title>
                     <Link to={`/comics/${comic.id}`}>
                       <Button variant='outline-info'>View it</Button>
                     </Link>
