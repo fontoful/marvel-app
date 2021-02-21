4335e73f9be585f6eaafaafe96c6defd327f0da7
Hector Serrano <fonto.serrano@gmail.com>
2021-02-20 23:14:06 -0600

    new helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,3 +4,11 @@ export const unifyString = (ob: { path: string; extension: string }): string =>
 export const capitalize = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const checkArrEmpty = (arr: string[]): boolean => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return true
+  }
+
+  return arr.filter(item => item !== '').length === 0
+}
