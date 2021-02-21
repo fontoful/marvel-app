46545e15a37c849feb021ba0d89c0a32e102ea6f
Hector Serrano <fonto.serrano@gmail.com>
2021-02-20 23:16:53 -0600

    searchParams logic

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom'
 import Navigation from '../components/Navigation'
 
 // React Bootstrap
-import { Card, Col, Container, Row, Spinner, Button } from 'react-bootstrap'
+import {
+  Card,
+  Col,
+  Container,
+  Row,
+  Spinner,
+  Button,
+  Form,
+} from 'react-bootstrap'
 
 //context
 import { GlobalContext } from '../context/GlobalContext'
@@ -18,12 +26,40 @@ import { IResponseAPI, IResultProps } from '../types/APItypes'
 const Characters: React.FC = () => {
   const [characters, setCharacters] = useState<IResultProps[]>([])
   const [loading, setLoading] = useState<boolean>(false)
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [key, setKey] = useState('nameStartsWith')
 
   const {
     addCharToBookmarks,
     characterBookmarks,
     removeCharFromBookmarks,
   } = useContext(GlobalContext)
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // prevent the default
+    e.preventDefault()
+
+    if (searchQuery === '') {
+      const resResults = await fetch(url('characters'))
+      const {
+        data: { results },
+      }: IResponseAPI = await resResults.json()
+      setCharacters(results)
+    } else {
+      const searchQueryParams = new URLSearchParams({ [key]: searchQuery })
+      const resResults = await fetch(
+        `${url('characters')}&${searchQueryParams.toString()}`,
+      )
+      const {
+        data: { results },
+      }: IResponseAPI = await resResults.json()
+      setCharacters(results)
+    }
+  }
 
   const checkBookmarkValidity = (id: number) => {
     const found = characterBookmarks.find(
@@ -71,6 +107,31 @@ const Characters: React.FC = () => {
   return (
     <Container>
       <Navigation liText='comics' bookmarks={true} />
+      <Row className='mt-2 mb-5 align-items-center justify-content-center'>
+        <Form inline onSubmit={onFormSubmit}>
+          <Form.Row>
+            <Col>
+              <Form.Control type='text' size='sm' onChange={onChangeHandler} />
+              <Form.Control
+                className='ml-3'
+                as='select'
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setKey(e.target.value)
+                }
+                custom
+              >
+                <option>nameStartsWith</option>
+                <option>comics</option>
+                <option>stories</option>
+              </Form.Control>
+
+              <Button type='submit' className='ml-2' variant='danger'>
+                Search
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+      </Row>
       <Row style={{ margin: 'auto' }}>
         {loading ? (
           <div className='mx-auto mt-5'>
